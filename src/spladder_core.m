2cbdb089a511a505c3f37c23d9e87070ed9ba425
Andre Kahles <akahles@cbio.mskcc.org>
2013-12-09 09:12:53 -0800

    improved loading behavior of existing files

--- src/spladder_core.m
+++ src/spladder_core.m
@@ -6,6 +6,8 @@ function spladder_core(CFG)
         addpath(CFG.paths{:});
     end;
 
+    genes_loaded = 0;
+
     %%% check if result file exists and start gen graph step if necessary
     if ~exist(CFG.out_fname, 'file'),
         fprintf('Augmenting splice graphs.\n');
@@ -19,56 +21,68 @@ function spladder_core(CFG)
 
         fprintf('Saving genes to %s\n', CFG.out_fname) ;
         save(CFG.out_fname, 'genes') ;
+
+        genes_loaded = 1;
     else
-        fprintf('Augmenting splice graphs already completed.\n%s exists\n ... loading genes from %s ...\n', CFG.out_fname, CFG.out_fname);
-        load(CFG.out_fname, 'genes') ;
-        fprintf('...done.\n\n');
+        fprintf('Augmenting splice graphs already completed.\n') ;
     end ;
 
+    %%% prune splice graph if necessary
     if CFG.do_prune,
+        load_fn = CFG.out_fname;
         CFG.out_fname = strrep(CFG.out_fname, '.mat', '_pruned.mat');
-    end;
-    if CFG.do_prune && ~exist(CFG.out_fname, 'file'),
-        genes = uniquify_splicegraph(genes);
+        if ~exist(CFG.out_fname, 'file'),
+            %%% load genes if not present yet
+            if genes_loaded == 0,
+                load(load_fn);
+            end;
 
-        %%% prune graphs
-        num_paths_before = count_all_paths(genes);
-        genes = prune_graph(genes, bam_fnames);
-        num_paths_after = count_all_paths(genes);
+            %%% make splice graphs unique
+            genes = uniquify_splicegraph(genes);
 
-        %%% save pruned genes
-        fprintf('saving genes to %s\n', CFG.out_fname) ;
-        save(CFG.out_fname, 'genes') ;
-    else
-        if CFG.do_prune,
-            fprintf('Pruning of splice graphs already done\n\tloading genes\n');
-            load(CFG.out_fname, 'genes') ;
+            %%% prune graphs
+            num_paths_before = count_all_paths(genes);
+            genes = prune_graph(genes, bam_fnames);
+            num_paths_after = count_all_paths(genes);
+
+            %%% save pruned genes
+            fprintf('saving genes to %s\n', CFG.out_fname) ;
+            save(CFG.out_fname, 'genes') ;
+
+            genes_loaded = 1;
         else
-            fprintf('No pruning requested!\n');
+            fprintf('Pruning of splice graphs already done\n');
         end;
+    else
+        fprintf('No pruning requested!\n');
     end;
 
+    %%% generate isoforms if necessary
     if CFG.do_gen_isoforms,
+        load_fn = CFG.out_fname;
         CFG.out_fame = strrep(CFG.out_fname, '.mat', '_with_isoforms.mat');
-    end;
-    if CFG.do_gen_isoforms && ~exist(CFG.out_fname, 'file')
-        %%% generate isoforms
-        fprintf('Generating all isoforms\n') ;
-        genes = generate_isoforms(genes, PAR.conf);
+        if ~exist(CFG.out_fname, 'file'),
+            %%% load genes if not present yet
+            if genes_loaded == 0,
+                load(load_fn);
+            end;
 
-        %%% re-constitute splicing graph
-        fprintf('\tRe-constituting simplified splice graph from generated transcripts\n') ;
-        genes_unsimplified = genes ;
-        genes = splice_graph(genes, conf.do_infer_splice_graph);
+            %%% generate isoforms
+            fprintf('Generating all isoforms\n') ;
+            genes = generate_isoforms(genes, PAR.conf);
 
-        %%% save splicing graph with isoforms
-        fprintf('\tSaving genes to %s\n', CFG.out_fname) ;
-        save(CFG.out_fname, 'genes', 'genes_unsimplified') ;
-    else
-        if CFG.do_gen_isoforms,
-            fprintf('Generating all isoforms already done\n\tloading genes\n');
+            %%% re-constitute splicing graph
+            fprintf('\tRe-constituting simplified splice graph from generated transcripts\n') ;
+            genes_unsimplified = genes ;
+            genes = splice_graph(genes, conf.do_infer_splice_graph);
+
+            %%% save splicing graph with isoforms
+            fprintf('\tSaving genes to %s\n', CFG.out_fname) ;
+            save(CFG.out_fname, 'genes', 'genes_unsimplified') ;
         else
-            fprintf('Generating all isoforms not requested\n');
+            fprintf('Generating all isoforms already done\n');
         end;
+    else
+        fprintf('Generating all isoforms not requested\n');
     end;
     fprintf('\n');
