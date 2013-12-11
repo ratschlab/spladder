85c49cb3a682dd119d341d7d36c167d680277beb
Andre Kahles <akahles@cbio.mskcc.org>
2013-12-10 20:22:08 -0800

    rename read filter

--- src/utils/make_introns_feasible.m
+++ src/utils/make_introns_feasible.m
@@ -13,9 +13,9 @@ function introns = make_introns_feasible(introns, genes, CFG)
 
     while ~isempty(unfeas),
         %%% make filter more stringent
-        CFG.intron_filter.exon_len = min(36, CFG.intron_filter.exon_len + 4);
-        CFG.intron_filter.mincount = CFG.intron_filter.mincount + 10;
-        CFG.intron_filter.mismatch = max(CFG.intron_filter.mismatch - 1, 0);
+        CFG.read_filter.exon_len = min(36, CFG.read_filter.exon_len + 4);
+        CFG.read_filter.mincount = CFG.read_filter.mincount + 10;
+        CFG.read_filter.mismatch = max(CFG.read_filter.mismatch - 1, 0);
 
         %%% get new intron counts
         tmp_introns = get_intron_list(genes(unfeas), CFG);
@@ -30,7 +30,7 @@ function introns = make_introns_feasible(introns, genes, CFG)
         end;
         still_unfeas = find(tmp1 > 200 | tmp2 > 200);
         for i = setdiff(unfeas, still_unfeas),
-            fprintf(CFG.fd_log, '[feasibility] set criteria for gene %s to: min_ex %i, min_conf %i, max_mism %i\n', genes(i).name, CFG.intron_filter.exon_len, CFG.intron_filter.mincount, CFG.intron_filter.mismatch);
+            fprintf(CFG.fd_log, '[feasibility] set criteria for gene %s to: min_ex %i, min_conf %i, max_mism %i\n', genes(i).name, CFG.read_filter.exon_len, CFG.read_filter.mincount, CFG.read_filter.mismatch);
         end;
         unfeas = still_unfeas;
     end;
