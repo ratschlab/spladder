cc9c090039985e0481bb381a29ee7a2a0fae8fe0
Andre Kahles <akahles@cbio.mskcc.org>
2013-12-11 11:43:47 -0800

    whitespace changes; used typecasting to prevent wrong usage in Octave

--- src/alt_splice/detect_multipleskips.m
+++ src/alt_splice/detect_multipleskips.m
@@ -1,24 +1,24 @@
 function [idx_multiple_skips, exon_multiple_skips, id_multiple_skips] = detect_multipleskips(genes, idx_alt) ;
 
-id=0 ;
+id = 0 ;
 idx_multiple_skips = zeros(1,0);
 id_multiple_skips = zeros(1,0);
 exon_multiple_skips = zeros(1,0) ;
-for ix=idx_alt 
-  if (mod(ix,50)==0)
-    fprintf(1,'.');
-  end
-  num_exons = size(genes(ix).splicegraph{1},2) ;
+for ix = idx_alt,
+  if (mod(ix, 50) == 0),
+    fprintf(1, '.');
+  end;
+  num_exons = size(genes(ix).splicegraph{1}, 2) ;
   edges = genes(ix).splicegraph{2} ;
   labels = repmat([1:num_exons]',1,num_exons) ;
   
   %adjecency matrix: upper half only
   A = sparse(zeros(num_exons)) ;
-  for i=1:num_exons-1
-    for j = i+1:num_exons
+  for i = 1:num_exons-1,
+    for j = i+1:num_exons,
       A(i,j) = edges(i,j) ;
-    end
-  end
+    end;
+  end;
   
   %possible starting and ending exons of a multiple exon skip
   Pairs = sparse(zeros(length(A))) ;
@@ -36,21 +36,20 @@ for ix=idx_alt
     continue ;
   end
   
-
   for cnt = 1:size(edge1,1)
     exon_idx_first = edge1(cnt) ;
     exon_idx_last = edge2(cnt) ;
 
-    
     if (edges(exon_idx_first,exon_idx_last))
       
       % find all pairs shortest path
-      exist_path = triu(edges);
+      exist_path = double(triu(edges));
       exist_path(exon_idx_first,exon_idx_last)=0;
       exist_path(exist_path==0)=inf ;
+      
       for i=1:num_exons; exist_path(i,i)=0 ; end ;
       
-      long_exist_path = -triu(edges);
+      long_exist_path = double(-triu(edges));
       long_exist_path(exon_idx_first,exon_idx_last)=0;
       long_exist_path(long_exist_path==0)=inf ;
       for i=1:num_exons; long_exist_path(i,i)=0 ; end ;
@@ -61,41 +60,41 @@ for ix=idx_alt
       
       for k=1:num_exons,
         for i=1:num_exons,
-          idx=find(exist_path(i,k)+exist_path(k,:)<exist_path(i,:));
-          exist_path(i,idx)=exist_path(i,k)+exist_path(k,idx) ;
+          idx = find((exist_path(i,k) + exist_path(k,:)) < exist_path(i,:));
+          exist_path(i,idx) = exist_path(i,k) + exist_path(k,idx) ;
           path(i,idx) = path(k,idx) ;
           
-          idx=find(long_exist_path(i,k)+long_exist_path(k,:)<long_exist_path(i,:));
-          long_exist_path(i,idx)=long_exist_path(i,k)+long_exist_path(k,idx);
+          idx = find((long_exist_path(i,k) + long_exist_path(k,:)) < long_exist_path(i,:));
+          long_exist_path(i,idx) = long_exist_path(i,k) + long_exist_path(k,idx);
           long_path(i,idx) = long_path(k,idx);
-        end 
-      end 
+        end ;
+      end ;
       
       temp_ix = ~isinf(long_exist_path);
       long_exist_path(temp_ix) = -long_exist_path(temp_ix);
       
-      if exist_path(exon_idx_first,exon_idx_last)>2 & ~isinf(exist_path(exon_idx_first,exon_idx_last))
+      if (exist_path(exon_idx_first,exon_idx_last) > 2) && ~isinf(exist_path(exon_idx_first,exon_idx_last)),
         backtrace = path(exon_idx_first,exon_idx_last);
         while (backtrace(end) > exon_idx_first)
           backtrace = [backtrace,path(exon_idx_first,backtrace(end))];
         end
         backtrace = backtrace(1:end-1);
         backtrace = backtrace(length(backtrace):-1:1);
-        idx_multiple_skips = [idx_multiple_skips,repmat(ix,1,length(backtrace)+2)];
+        idx_multiple_skips = [idx_multiple_skips, repmat(ix,1,length(backtrace)+2)];
         exon_multiple_skips = [exon_multiple_skips, exon_idx_first backtrace exon_idx_last];
-        id=id+1 ;
+        id = id + 1 ;
         id_multiple_skips = [id_multiple_skips,id*ones(1,length(backtrace)+2)];
-      elseif long_exist_path(exon_idx_first,exon_idx_last)>2 & ~isinf(long_exist_path(exon_idx_first,exon_idx_last))
+      elseif (long_exist_path(exon_idx_first,exon_idx_last) > 2) && ~isinf(long_exist_path(exon_idx_first,exon_idx_last)),
         backtrace = long_path(exon_idx_first,exon_idx_last);
         while (backtrace(end) > exon_idx_first)
-          backtrace = [backtrace,long_path(exon_idx_first,backtrace(end))];
+          backtrace = [backtrace, long_path(exon_idx_first, backtrace(end))];
         end
         backtrace = backtrace(1:end-1);
         backtrace = backtrace(length(backtrace):-1:1);
-        idx_multiple_skips = [idx_multiple_skips,repmat(ix,1,length(backtrace)+2)];
+        idx_multiple_skips = [idx_multiple_skips,repmat(ix, 1, length(backtrace)+2)];
         exon_multiple_skips = [exon_multiple_skips, exon_idx_first backtrace exon_idx_last];
-        id=id+1 ;
-        id_multiple_skips = [id_multiple_skips,id*ones(1,length(backtrace)+2)];
+        id = id + 1 ;
+        id_multiple_skips = [id_multiple_skips, id * ones(1, length(backtrace)+2)];
       end
       %if exist_path(exon_idx_first,exon_idx_last) < ...
       %      long_exist_path(exon_idx_first,exon_idx_last)
