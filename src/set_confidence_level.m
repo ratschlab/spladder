05f19199afb720c58803ff63f9b6e0343672ea1d
Andre Kahles <akahles@cbio.mskcc.org>
2013-12-11 11:59:50 -0800

    fixed function name; made read filter dependent on read length

--- src/set_confidence_level.m
+++ src/set_confidence_level.m
@@ -1,26 +1,26 @@
-function CFG = set_confidence_level_level(CFG),
-%function CFG = set_confidence_level_level(CFG),
+function CFG = set_confidence_level(CFG),
+%function CFG = set_confidence_level(CFG),
 
     %%% settings for accepted introns
     CFG.read_filter = [] ;
     if CFG.confidence_level == 0,
         CFG.read_filter.intron = 20000 ; 
-        CFG.read_filter.exon_len = 8;
-        CFG.read_filter.mismatch = 2 ;
+        CFG.read_filter.exon_len = ceil(CFG.read_length * 0.10);
+        CFG.read_filter.mismatch = floor(CFG.read_length * 0.02) ;
         CFG.read_filter.mincount = 1 ;
     elseif CFG.confidence_level == 1,
         CFG.read_filter.intron = 20000 ; 
-        CFG.read_filter.exon_len = 12;
-        CFG.read_filter.mismatch = 1 ;
-        CFG.read_filter.mincount = 2 ;
+        CFG.read_filter.exon_len = ceil(CFG.read_length * 0.15); %12;
+        CFG.read_filter.mismatch = floor(CFG.read_length * 0.01); %1 ;
+        CFG.read_filter.mincount = 2;
     elseif CFG.confidence_level == 2
         CFG.read_filter.intron = 20000 ; 
-        CFG.read_filter.exon_len = 20;
-        CFG.read_filter.mismatch = 1 ;
+        CFG.read_filter.exon_len = ceil(CFG.read_length * 0.20);
+        CFG.read_filter.mismatch = floor(CFG.read_length * 0.01) ;
         CFG.read_filter.mincount = 5 ;
     elseif CFG.confidence_level == 3
         CFG.read_filter.intron = 20000 ; 
-        CFG.read_filter.exon_len = 25;
+        CFG.read_filter.exon_len = ceil(CFG.read_length * 0.25);
         CFG.read_filter.mismatch = 0 ;
         CFG.read_filter.mincount = 10 ;
     end ;
