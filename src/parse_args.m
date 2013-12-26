485e45b1a60474ecfc29e00031a877165c633adf
Andre Kahles <akahles@cbio.mskcc.org>
2013-12-26 14:12:30 -0800

    use variable names as strings in test for existence

--- src/parse_args.m
+++ src/parse_args.m
@@ -1,4 +1,15 @@
-function CFG = parse_args(ARGS, CFG)
+function CFG = parse_args(ARGS, CFG_)
+
+%%% load all default settings
+default_settings
+
+%%% take care of xisting configurations
+if nargin > 1,
+    fn = fieldnames(CFG_);
+    for i = 1:length(fn),
+        CFG.(fn{i}) = CFG_.(fn{i});
+    end;
+end;
 
 %%% turn off warnings
 if IS_OCT,
@@ -15,9 +26,6 @@ if IS_OCT,
 else
     warning('off', 'MATLAB:typeaheadBufferOverflow');
 end;
-
-%%% load all default settings
-default_settings
 
 %%% Split Parameter string
 ARGS = split_string(ARGS, ';');
@@ -53,38 +61,38 @@ for i = 1:length(ARGS),
 end;
 
 %%% get switches
-if exist(F_INSERT_IR, 'var') == 1, CFG.do_insert_intron_retentions = F_INSERT_IR; end;
-if exist(F_INSERT_CE, 'var') == 1, CFG.do_insert_cassette_exons = F_INSERT_CE; end;
-if exist(F_INSERT_IE, 'var') == 1, CFG.do_insert_intron_edges = F_INSERT_IE; end;
-if exist(F_REMOVE_SE, 'var') == 1, CFG.do_remove_short_exons = F_REMOVE_SE; end;
-if exist(F_INFER_SG, 'var') == 1, CFG.do_infer_splice_graph = F_INFER_SG; end;
-if exist(F_VERBOSE, 'var') == 1, CFG.verbose = F_VERBOSE; end;
-if exist(F_DEBUG, 'var') == 1, CFG.debug = F_DEBUG; end;
+if exist('F_INSERT_IR', 'var') == 1, CFG.do_insert_intron_retentions = F_INSERT_IR; end;
+if exist('F_INSERT_CE', 'var') == 1, CFG.do_insert_cassette_exons = F_INSERT_CE; end;
+if exist('F_INSERT_IE', 'var') == 1, CFG.do_insert_intron_edges = F_INSERT_IE; end;
+if exist('F_REMOVE_SE', 'var') == 1, CFG.do_remove_short_exons = F_REMOVE_SE; end;
+if exist('F_INFER_SG', 'var') == 1, CFG.do_infer_splice_graph = F_INFER_SG; end;
+if exist('F_VERBOSE', 'var') == 1, CFG.verbose = F_VERBOSE; end;
+if exist('F_DEBUG', 'var') == 1, CFG.debug = F_DEBUG; end;
 
-if exist(I_INSERT_INTRON_ITER, 'var') == 1, CFG.insert_intron_iterations = I_INSERT_INTRON_ITER; end;
-if exist(I_CONFIDENCE, 'var') == 1, CFG.confidence_level = I_CONFIDENCE; end;
+if exist('I_INSERT_INTRON_ITER', 'var') == 1, CFG.insert_intron_iterations = I_INSERT_INTRON_ITER; end;
+if exist('I_CONFIDENCE', 'var') == 1, CFG.confidence_level = I_CONFIDENCE; end;
 
 %%% settings for the alt splice part
-if exist(S_MERGE_STRATEGY, 'var') == 1, CFG.merge_strategy = S_MERGE_STRATEGY; end;
-if exist(F_VALIDATE_SG, 'var') == 1, CFG.validate_splicegraphs = F_VALIDATE_SG; end;
-if exist(F_SHARE_GENESTRUCT, 'var') == 1, CFG.same_genestruct_for_all_samples = F_SHARE_GENESTRUCT; end;
-if exist(S_REPLICATE_IDX, 'var') == 1, CFG.replicate_idxs = split_string(S_REPLICATE_IDX, ','); end;
-if exist(F_CURATE_ALTPRIME, 'var') == 1, CFG.curate_alt_prime_events = F_CURATE_ALTPRIME; end;
+if exist('S_MERGE_STRATEGY', 'var') == 1, CFG.merge_strategy = S_MERGE_STRATEGY; end;
+if exist('F_VALIDATE_SG', 'var') == 1, CFG.validate_splicegraphs = F_VALIDATE_SG; end;
+if exist('F_SHARE_GENESTRUCT', 'var') == 1, CFG.same_genestruct_for_all_samples = F_SHARE_GENESTRUCT; end;
+if exist('S_REPLICATE_IDX', 'var') == 1, CFG.replicate_idxs = split_string(S_REPLICATE_IDX, ','); end;
+if exist('F_CURATE_ALTPRIME', 'var') == 1, CFG.curate_alt_prime_events = F_CURATE_ALTPRIME; end;
 
 %%% open log file, if specified
-if exist(S_LOG_FNAME, 'var') == 1,
+if exist('S_LOG_FNAME', 'var') == 1,
     CFG.log_fname = S_LOG_FNAME;
     CFG.fd_log = fopen(S_LOG_FNAME, 'w');
 end;
 
-if exist(S_USER_FNAME, 'var') == 1, CFG.user_settings = S_USER_FNAME; end;
+if exist('S_USER_FNAME', 'var') == 1, CFG.user_settings = S_USER_FNAME; end;
 
 %%% mandatory parameters
-CFG.bam_fnames = split_string(S_BAM_FNAME, ',');
-CFG.anno_fname = S_ANNO_FNAME;
-CFG.out_dirname = S_OUT_DIRNAME;
+if exist('S_BAM_FNAME', 'var') == 1, CFG.bam_fnames = split_string(S_BAM_FNAME, ','); end;
+if exist('S_ANNO_FNAME', 'var') == 1, CFG.anno_fname = S_ANNO_FNAME; end;
+if exist('S_OUT_DIRNAME', 'var') == 1, CFG.out_dirname = S_OUT_DIRNAME; end;
 
-if exist(S_REFERENCE_STRAIN, 'var') == 1,
+if exist('S_REFERENCE_STRAIN', 'var') == 1,
     CFG.reference_strain = S_REFERENCE_STRAIN;
     ref_tag = sprintf('%s:', S_REFERENCE_STRAIN);
 else
@@ -93,8 +101,9 @@ end;
 
 CFG.list_config = {};
 CFG.samples = {};
+CFG.strains = {};
 for i = 1:length(CFG.bam_fnames),
-    if exist(S_EXPERIMENT_LABEL, 'var') == 1,
+    if exist('S_EXPERIMENT_LABEL', 'var') == 1,
         CFG.samples{end + 1} = [S_EXPERIMENT_LABEL '_' regexprep(regexprep(CFG.bam_fnames{i}, '.*/', ''), '.bam', '')]; 
     else
         CFG.samples{end + 1} = [regexprep(regexprep(CFG.bam_fnames{i}, '.*/', ''), '.bam', '')]; 
@@ -103,7 +112,7 @@ for i = 1:length(CFG.bam_fnames),
 end;
 
 %%% rproc options
-if exist(F_PROC, 'var') == 1,
+if exist('F_PROC', 'var') == 1,
     CFG.rproc = F_RPROC;
     CFG.options_rproc = struct()
     CFG.options_rproc.mem_req_resubmit  = [30000 60000 80000];
