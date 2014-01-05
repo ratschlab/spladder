782c10b1eb804a00d23d4117068b5cf8e78b209e
Andre Kahles <akahles@cbio.mskcc.org>
2014-01-05 10:52:54 -0800

    adapted from rDiff to Spladder after copying

--- bin/start_interpreter.sh
+++ bin/start_interpreter.sh
@@ -12,20 +12,17 @@
 
 set -e
 
-. `dirname $0`/rdiff_config.sh
+. `dirname $0`/spladder_config.sh
 
-#export MATLAB_RETURN_FILE=`tempfile`
-export MATLAB_RETURN_FILE=`mktemp -t rDiff.XXXXXXXXXX.tmp` 
+export MATLAB_RETURN_FILE=`mktemp -t spladder.XXXXXXXXXX.tmp` 
 
 
-if [ "$RDIFF_INTERPRETER" == 'octave' ];
+if [ "$SPLADDER_INTERPRETER" == 'octave' ];
 then
-	echo exit | ${RDIFF_OCTAVE_BIN_PATH} -q --eval "global SHELL_INTERPRETER_INVOKE; SHELL_INTERPRETER_INVOKE=1; warning('off', 'Octave:shadowed-function'); warning('off', 'Octave:deprecated-function') ; addpath $RDIFF_SRC_PATH;  $1('$2'); exit;" || (echo starting Octave failed; rm -f $MATLAB_RETURN_FILE; exit -1) ;
-fi
-
-if [ "$RDIFF_INTERPRETER" == 'matlab' ];
+	echo exit | ${SPLADDER_OCTAVE_BIN_PATH} -q --eval "global SHELL_INTERPRETER_INVOKE; SHELL_INTERPRETER_INVOKE=1; warning('off', 'Octave:shadowed-function'); warning('off', 'Octave:deprecated-function') ; addpath $SPLADDER_SRC_PATH;  $1('$2'); exit;" || (echo starting Octave failed; rm -f $MATLAB_RETURN_FILE; exit -1) ;
+elif [ "$SPLADDER_INTERPRETER" == 'matlab' ];
 then
-	echo exit | ${RDIFF_MATLAB_BIN_PATH} -nodisplay -r "global SHELL_INTERPRETER_INVOKE; SHELL_INTERPRETER_INVOKE=1; addpath $RDIFF_SRC_PATH;  $1('$2'); exit;" || (echo starting Matlab failed; rm -f $MATLAB_RETURN_FILE; exit -1) ;
+	echo exit | ${SPLADDER_MATLAB_BIN_PATH} -nodisplay -r "global SHELL_INTERPRETER_INVOKE; SHELL_INTERPRETER_INVOKE=1; addpath $SPLADDER_SRC_PATH;  $1('$2'); exit;" || (echo starting Matlab failed; rm -f $MATLAB_RETURN_FILE; exit -1) ;
 fi
 
 test -f $MATLAB_RETURN_FILE || exit 0
