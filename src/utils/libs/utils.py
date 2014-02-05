0838436d693608582338e098590ae78066d5ee87
Andre Kahles <akahles@cbio.mskcc.org>
2014-02-05 07:40:21 -0800

    removed py files from master branch

--- src/utils/libs/utils.py
+++
@@ -1,8 +0,0 @@
-def unique_rows(array):
-    """Make array unique by rows"""
-
-    if array.shape[0] == 0:
-        return array
-
-    array_u = np.unique(array.view([('', array.dtype)] * array.shape[1]))
-    return array_u.view(array.dtype).reshape(array_u.shape[0], array.shape[1])
