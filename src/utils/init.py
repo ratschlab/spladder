b877e68ff4d056717adad2f4e5486afc8e84ee81
Andre Kahles <akahles@cbio.mskcc.org>
2014-02-26 11:49:38 -0800

    moved code into module

--- src/utils/init.py
+++ src/utils/init.py
@@ -1,5 +1,7 @@
 import sys
 import cPickle
+import os
+import pysam
 import scipy as sp
 from gene import Gene
 from splicegraph import Splicegraph
@@ -106,6 +108,35 @@ def init_genes_gff3(infile, CFG=None, outfile=None):
 
     return genes
 
-if __name__ == "__main__":
-    #tut = init_genes_gff3('/cbio/grlab/projects/TCGA/PanCancer/annotation/gencodeV14.v7.debug.gff3', CFG={'verbose':True}, outfile='test.genes.pickle')
-    tut = init_genes_gff3('/cbio/grlab/projects/TCGA/PanCancer/annotation/gencodeV14.v7.gff3', CFG={'verbose':True}, outfile='test.genes.pickle')
+def init_regions(fn_bams):
+    # regions=init_regions(fn_bams)
+
+    regions = []
+    processed = []
+    cnt = 0
+
+    for i in range(len(fn_bams)):
+        if not os.path.exists(fn_bams[i]):
+            continue
+        else:
+            ### load bamfile
+            IN = pysam.Samfile(fn_bams[i], 'rb')
+            header_info = IN.header['SQ']
+            strands = ['+', '-']
+            for c in range(len(header_info)):
+                if not (header_info[c]['SN'], header_info[c]['LN']) in processed:
+                    for s in strands:
+                        region = Region()
+                        region.chr = header_info[c]['SN'])
+                        region.chr_num = c
+                        region.strand = s
+                        region.start = 1
+                        region.stop = header_info[c]['LN']
+                        region.id = cnt
+                        regions.append(region)
+                        cnt += 1
+                    processed.append((header_info[c]['SN'], header_info[c]['LN']))
+            IN.close()
+        break
+
+    return sp.array(regions, dtype = 'object')
