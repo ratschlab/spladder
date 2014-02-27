34609ae0e2bf46ab69a29df94ebe324ab8a7bddb
Andre Kahles <akahles@cbio.mskcc.org>
2014-02-27 10:06:06 -0800

    moved code into gen_graphs

--- src/utils/gen_graphs.py
+++ src/utils/gen_graphs.py
@@ -18,7 +18,27 @@ def gen_graphs(genes, CFG=None):
     # build splice graph for all genes 
     ##############################################################################%%
     print >> CFG['fd_log'], 'Generating splice graph ...'
-    genes = splice_graph(genes, CFG)
+	### merge exons if possible / reduce graph
+    ### originially implemented for ESTs, reduces complexity, 
+    ### but removes alternative transcript starts and ends !
+	if CFG['do_infer_splice_graph']
+		genes = infer_splice_graph(genes)
+
+	### sort exons by start position in ascending order
+	for ix in rang(genes.shape[0]):
+        genes[ix].splicegraph.sort()
+
+	### label alternative and constitutive genes
+	genes = label_alt_genes(genes, CFG)
+
+    ### update terminals, start terminals in row 1, end terminals in row 2
+	for i in range(genes.shape[0]):
+        genes[i].splicegraph.update_terminals()
+
+	### reset gene start and gene stop according to exons and splice graph
+	for i in range(genes.shape[0]):
+        genes[i].start = min([x.min() for x in genes[j].exons])
+        genes[i].stop = max([x.max() for x in genes[j].exons])
     print >> CFG['fd_log'], '...done.\n'
 
     # append list of introns supported by RNA-seq data to 
