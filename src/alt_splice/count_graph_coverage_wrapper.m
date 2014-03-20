84aa92c9f67fd29db465bfdb858b785afb3470b6
Andre Kahles <akahles@cbio.mskcc.org>
2014-03-20 16:37:11 -0700

    altered procedure how counts are stored to be more memory efficient

--- src/alt_splice/count_graph_coverage_wrapper.m
+++ src/alt_splice/count_graph_coverage_wrapper.m
@@ -2,18 +2,18 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
 
     load(fname_in);
     
-    %if ~isfield(genes, 'segmentgraph'),
+    if ~isfield(genes, 'segmentgraph'),
         genes = create_segment_graph(genes, CFG);
         secsave(fname_in, genes, 'genes');
-    %end;
+    end;
 
-    if CFG.rproc,
+    if ~CFG.rproc,
         for s_idx = 1:length(CFG.strains),
             fprintf('%i/%i\r', j, length(CFG.strains));
-            counts = count_graph_coverage(genes, CFG.list_bam(s_idx), CFG);
+            counts = count_graph_coverage(genes, CFG.bam_fnames(s_idx), CFG);
         end ;
     else
-        chunksize = 10;
+        chunksize = 2;
         jobinfo = rproc_empty(0) ;
         PAR = struct();
         PAR.CFG = CFG;
@@ -28,29 +28,44 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
                 PAR.fn_bam = CFG.bam_fnames;
                 PAR.fn_out = fn;
                 PAR.CFG = CFG;
-                %jobinfo(end + 1) = rproc('count_graph_coverage', PAR, 30000, CFG.options_rproc, 48*60);
-                count_graph_coverage(PAR);
+                jobinfo(end + 1) = rproc('count_graph_coverage', PAR, 30000, CFG.options_rproc, 48*60);
+                %count_graph_coverage(PAR);
             end;
         end;
 
         jobinfo = rproc_wait(jobinfo, 30, 1, 1) ;
 
         %%% merge results
-        counts = struct();
+        counts_segments = {};
+        counts_seg_pos = {};
+        counts_edges = {};
+        %counts = struct();
         for c_idx = 1:chunksize:length(genes),
+            fprintf('%i/%i\r', c_idx, length(genes));
             cc_idx = min(length(genes), c_idx + chunksize - 1);
             fn = strrep(fname_out, '.mat', sprintf('.chunk_%i_%i.mat', c_idx, cc_idx));
             if ~exist(fn, 'file'),
                 error('Not all chunks in counting graph coverage completed!');
             else
                 L = load(fn);
-                if all(size(counts) == 1),
-                    counts = L.counts;
-                else
-                    counts(:, c_idx:cc_idx) = L.counts;
+                for c = 0:chunksize-1,
+                    if c_idx + c <= length(genes),
+                        counts_segments{c_idx + c} = vertcat(L.counts(:, c+1).segments);
+                        counts_seg_pos{c_idx + c} = vertcat(L.counts(:, c+1).seg_pos);
+                        tmp = [L.counts(:, c+1).edges];
+                        if ~isempty(tmp),
+                            counts_edges{c_idx + c} = [tmp(:, 1), tmp(:, 2:2:end)];
+                        end;
+                    end;
+               % if isempty(counts_segments),
+               %     counts_segments{end + 1} = 
+               % if all(size(counts) == 1),
+               %     counts = L.counts;
+               % else
+               %     counts(:, c_idx:cc_idx) = L.counts;
                 end;
             end;
         end;
     end;
 
-    save(fname_out, 'counts');
+    secsave(fname_out, {counts_segments, counts_seg_pos, counts_edges}, {'counts_segments', 'counts_seg_pos', 'counts_edges'});
