da4c6b60ea9f154c1dd3c3e69d6c4b5d317296b6
Andre Kahles <akahles@cbio.mskcc.org>
2014-06-05 15:26:05 -0700

    fixed bug in filter by edgecount that caused single exons to disappear

--- src/utils/filter_by_edgecount.m
+++ src/utils/filter_by_edgecount.m
@@ -1,4 +1,5 @@
 function filter_by_edgecount(CFG, fn_genes, fn_out)
+% function filter_by_edgecount(CFG, fn_genes, fn_out)
 
     %%% load gene structure
     fprintf(1, 'Loading merged genes from %s ...\n', fn_genes);
@@ -7,7 +8,9 @@ function filter_by_edgecount(CFG, fn_genes, fn_out)
 
     %%% filter splicegraphs by support count over samples
     for i = 1:length(genes),
-        k_idx = find(sum(genes(i).splicegraph{2}, 2) == 0);
+        u_exons = unique(vertcat(genes(i).exons{:}), 'rows');
+        [tmp, tmp, k_idx] = intersect(u_exons, genes(i).splicegraph{1}', 'rows');
+        %k_idx = find(sum(genes(i).splicegraph{2}, 2) == 0);
         genes(i).splicegraph{2} = genes(i).edge_count >= CFG.sg_min_edge_count;
         %%% remove all exons that have no incoming or outgoing edges (caused by validation, keep single exon transcripts that occured before)
         rm_idx = setdiff(find(sum(genes(i).splicegraph{2}, 2) == 0), k_idx);
@@ -18,10 +21,12 @@ function filter_by_edgecount(CFG, fn_genes, fn_out)
             if length(genes(i).splicegraph) > 2,
                 genes(i).splicegraph{3}(:, rm_idx) = [];
             end;
+            genes(i).edge_count(:, rm_idx) = [];
+            genes(i).edge_count(rm_idx, :) = [];
         end;
     end;
 
-    genes = rmfield(genes, 'edge_count');
+    %genes = rmfield(genes, 'edge_count');
     %%% saving validated splicegraph
     fprintf(1, 'Saving validated genes to: %s\n', fn_out);
     save(fn_out, 'genes');
