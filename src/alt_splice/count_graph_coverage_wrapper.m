9e58ed3bdf33ad5bbd818d456c944c3c24af56d9
Andre Kahles <akahles@cbio.mskcc.org>
2014-06-05 15:14:13 -0700

    allow flexible chunking, write more data into hdf5

--- src/alt_splice/count_graph_coverage_wrapper.m
+++ src/alt_splice/count_graph_coverage_wrapper.m
@@ -28,7 +28,8 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
             end;
         end;
     else
-        chunksize = 2;
+        %%% have an adaptive chunk size, that takes into account the number of strains (take as many genes as it takes to have ~10K strains)
+        chunksize = max(1, floor(10000 / length(CFG.strains)));
         jobinfo = rproc_empty(0) ;
         PAR = struct();
         PAR.CFG = CFG;
@@ -44,12 +45,12 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
                 PAR.fn_bam = CFG.bam_fnames;
                 PAR.fn_out = fn;
                 PAR.CFG = CFG;
-                jobinfo(end + 1) = rproc('count_graph_coverage', PAR, 8000, CFG.options_rproc, 48*60);
+                jobinfo(end + 1) = rproc('count_graph_coverage', PAR, 8000, CFG.options_rproc, 60);
                 %count_graph_coverage(PAR);
             end;
         end;
 
-        jobinfo = rproc_wait(jobinfo, 30, 1, 1) ;
+        jobinfo = rproc_wait(jobinfo, 30, 1, -1) ;
 
         %%% merge results
         for c_idx = 1:chunksize:length(genes),
@@ -84,6 +85,40 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
     counts.edges = vertcat(counts.edges{:});
     counts.edge_idx = counts.edges(:, 1);
     counts.edges = counts.edges(:, 2:end);
+    seg_lens = [];
+    if CFG.is_half_open,
+        ho_offset = 1;
+    else
+        ho_offset = 1;
+    end;
+    for i = 1:length(genes),
+        if ~isempty(genes(i).segmentgraph{1}),
+            seg_lens = [seg_lens, genes(i).segmentgraph{1}(2, :) - genes(i).segmentgraph{1}(1, :) + 1 - ho_offset];
+        end;
+    end;
+
+    %%% write gene_names to hdf5
+    h5fid = H5F.create(fname_out, 'H5F_ACC_TRUNC', 'H5P_DEFAULT', 'H5P_DEFAULT');
+    type_id = H5T.copy('H5T_C_S1');
+    H5T.set_size(type_id, 'H5T_VARIABLE');
+    space_id = H5S.create_simple(1, [length(genes)], H5ML.get_constant_value('H5S_UNLIMITED'));
+    plist = H5P.create('H5P_DATASET_CREATE');
+    H5P.set_chunk(plist,2); % 2 strings per chunk
+    dset_id = H5D.create(h5fid, 'gene_names', type_id, space_id, plist);
+    H5D.write(dset_id, type_id, 'H5S_ALL', 'H5S_ALL','H5P_DEFAULT', {genes.name});
+    H5S.close(space_id);
+    H5D.close(dset_id);
+
+    %%% write strains to hdf5
+    space_id = H5S.create_simple(1, [length(CFG.strains)], H5ML.get_constant_value('H5S_UNLIMITED'));
+    plist = H5P.create('H5P_DATASET_CREATE');
+    H5P.set_chunk(plist,2); % 2 strings per chunk
+    dset_id = H5D.create(h5fid, 'strains', type_id, space_id, plist);
+    H5D.write(dset_id, type_id, 'H5S_ALL', 'H5S_ALL','H5P_DEFAULT', CFG.strains);
+    H5S.close(space_id);
+    H5D.close(dset_id);
+
+    H5F.close(h5fid);
 
     %%% write data to HDF5
     h5create(fname_out, '/gene_ids_segs', size(counts.gene_ids_segs), 'ChunkSize', min([size(counts.gene_ids_segs); [100 1]], [], 1), 'Deflate', 6);
@@ -98,5 +133,8 @@ function count_graph_coverage_wrapper(fname_in, fname_out, CFG)
     h5write(fname_out, '/edges', counts.edges);
     h5create(fname_out, '/edge_idx', size(counts.edge_idx), 'Chunksize', min([size(counts.edge_idx); [100, 1]], [], 1), 'Deflate', 6);
     h5write(fname_out, '/edge_idx', counts.edge_idx);
+    h5create(fname_out, '/seg_len', size(seg_lens), 'Chunksize', min([size(seg_lens); [100, 50]], [], 1), 'Deflate', 6);
+    h5write(fname_out, '/seg_len', seg_lens);
+
 
     %secsave(fname_out, counts, 'counts');
