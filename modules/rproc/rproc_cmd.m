e7e0a6f65083af3beaab1e83a127900a58a98a54
Andre Kahles <akahles@cbio.mskcc.org>
2014-11-29 18:25:24 -0500

    replaced rproc matlab code by python module

--- modules/rproc/rproc_cmd.m
+++
@@ -1,9 +0,0 @@
-function rproc_cmd(unix_cmd, jobinfo)
-% rproc_cmd(unix_cmd, jobinfo)
-
-for i=1:length(jobinfo(:)),
-  if ~isempty(jobinfo(i).jobid) && jobinfo(i).jobid~=-1,
-    unix(sprintf('%s %i', unix_cmd, jobinfo(i).jobid)) ;
-    %assert(s==0) ;
-  end ;
-end ;
