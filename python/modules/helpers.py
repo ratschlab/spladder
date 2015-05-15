20a677d559eac002bbd1737fb197de1c8afb932d
Andre Kahles <akahles@cbio.mskcc.org>
2015-05-15 07:38:50 -0400

    added new function to assemble filenames

--- python/modules/helpers.py
+++ python/modules/helpers.py
@@ -36,3 +36,32 @@ def make_introns_feasible(introns, genes, CFG):
         unfeas = still_unfeas;
 
     return introns
+
+### determine count output file
+def get_filename(which, CFG):
+    """This function returns a filename generated from the current configuration"""
+
+    ### init any tags
+    prune_tag = ''
+    if CFG['do_prune']:
+        prune_tag = '_pruned'
+
+    ### iterate over return file types    
+    if which in ['fn_count_in', 'fn_count_out']:
+        if not 'spladder_infile' in CFG:
+            if CFG['validate_splicegraphs']:
+                fname = os.path.join(CFG['out_dirname'], 'spladder', 'genes_graph_conf%i.%s%s.validated.pickle' % (CFG['confidence_level'], CFG['merge_strategy'], prune_tag))
+            else:
+                fname = os.path.join(CFG['out_dirname'], 'spladder', 'genes_graph_conf%i.%s%s.pickle' % (CFG['confidence_level'], CFG['merge_strategy'], prune_tag))
+        else:
+            fname = CFG['spladder_infile']
+        
+        if which == 'fn_count_in':
+            return fname
+        elif which == 'fn_count_out':
+            return fname.replace('.pickle', '') + '.count.pickle'
+    elif which == 'fn_out_merge':
+        if CFG['merge_strategy'] == 'merge_graphs':
+            return os.path.join(CFG['out_dirname'], 'spladder', 'genes_graph_conf%i.%s%s.pickle' % (CFG['confidence_level'], CFG['merge_strategy'], prune_tag))
+        else:
+            return ''
