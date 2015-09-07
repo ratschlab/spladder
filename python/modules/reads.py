1a3abba9af9d1486b138f87f30fc6a4bfa79456e
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2015-09-06 22:56:48 -0500

    reconfigured get_intron_list to process chunks in parallel; added a generator function and a process_chunk function to accomplish this

--- python/modules/reads.py
+++ python/modules/reads.py
@@ -335,6 +335,7 @@ def __process_chunk(*args):
     introns = sort_rows(intron_list_tmp[0])
     num_introns_filtered = intron_list_tmp[0].shape[0]
     del chunk, s, strand, chr, chr_num, bam_args, gene, intron_list_tmp
+
     return [chunk_idx, c, num_introns_filtered, introns]
 
 def __chunk_generator(chunks, chr_num, s, c):
@@ -385,7 +386,14 @@ def get_intron_list(genes, CFG):
         s = strands.index(regions[j].strand)
         
     	if CFG['parallel'] > 1:
+<<<<<<< HEAD
 	    pool = CFG['pool']
+=======
+	    import multiprocessing as mp
+	    import signal as sig
+	    pool = mp.Pool(processes=CFG['parallel'], 
+				initializer=lambda: sig.signal(sig.SIGINT, sig.SIG_IGN))
+>>>>>>> reconfigured get_intron_list to process chunks in parallel; added a generator function and a process_chunk function to accomplish this
 	    
 	    try:
 	        result = [pool.apply_async(__process_chunk, args=(
@@ -408,18 +416,45 @@ def get_intron_list(genes, CFG):
 		sys.exit(1)
 
 	else:
+<<<<<<< HEAD
 	    for i in __chunk_generator(chunks, chr_num, s, c):
+=======
+	    while c[0] < chunks.shape[0]:
+		# fill the chunks on the corresponding chromosome
+        	i = c[0]
+        	if chunks[i, 0] > chr_num or chunks[i, 1] > s:
+            	    break
+        	if chunks[i, 0] != chr_num:
+            	    raise Exception('ERROR: c logic seems wrong')
+
+>>>>>>> reconfigured get_intron_list to process chunks in parallel; added a generator function and a process_chunk function to accomplish this
         	if CFG['verbose'] and (i+1) % 100 == 0:
          	    t1 = time.time()
          	    print >> sys.stdout, '%i (%i) genes done (%i introns taken) ... took %i secs' % (i+1, chunks.shape[0], num_introns_filtered, t1 - t0)
             	    t0 = t1
 
+<<<<<<< HEAD
 	        args = [ chunks[i], chunk_idx[i], i, s, strand, chr, chr_num, bam_args, genes[chunk_idx[i]] ]
 		result = __process_chunk(args)
 		idx = result[0]
 		num_introns_filtered += result[2]
 		introns[idx, s] = result[3]
     
+=======
+	        gg = sp.array([copy.copy(genes[chunk_idx[i]])], dtype='object')
+        	gg[0].strand = strands[s]
+        	gg[0].start = max(gg[0].start - 5000, 1)
+      		gg[0].stop = gg[0].stop + 5000
+        	assert(gg[0].chr == chr)
+
+        	intron_list_tmp = add_reads_from_bam(gg, CFG['bam_fnames'], ['intron_list'], CFG['read_filter'], CFG['var_aware'], CFG['primary_only'])
+        	num_introns_filtered += intron_list_tmp[0].shape[0]
+        	introns[chunk_idx[i], s] = sort_rows(intron_list_tmp[0])
+
+		c[0] += 1
+    
+
+>>>>>>> reconfigured get_intron_list to process chunks in parallel; added a generator function and a process_chunk function to accomplish this
     for j in range(introns.shape[0]):
         if introns[j, 0] is None:
             introns[j, 0] = sp.zeros((0, 3), dtype='int')
