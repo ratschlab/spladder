160f22551b236adbd94517b22eca522b7d38fa2c
Andre Kahles <akahles@cbio.mskcc.org>
2016-02-15 11:14:56 -0500

    use invocation of parallel detect

--- python/modules/alt_splice/collect.py
+++ python/modules/alt_splice/collect.py
@@ -107,7 +107,7 @@ def collect_events(CFG):
                 ### detect intron retentions from splicegraph
                 if do_intron_retention:
                     if not os.path.exists(fn_out_ir):
-                        idx_intron_reten, intron_intron_reten = detect_intronreten(genes, sp.where([x.is_alt for x in genes])[0])
+                        idx_intron_reten, intron_intron_reten = detect_events(genes, 'intron_retention', sp.where([x.is_alt for x in genes])[0], CFG)
                         for k in range(len(idx_intron_reten)):
                             gene = genes[idx_intron_reten[k]]
 
@@ -142,7 +142,7 @@ def collect_events(CFG):
                 ### detect exon_skips from splicegraph
                 if do_exon_skip:
                     if not os.path.exists(fn_out_es):
-                        idx_exon_skip, exon_exon_skip = detect_exonskips(genes, sp.where([x.is_alt for x in genes])[0])
+                        idx_exon_skip, exon_exon_skip = detect_events(genes, 'exon_skip', sp.where([x.is_alt for x in genes])[0], CFG)
                         for k in range(len(idx_exon_skip)):
                             gene = genes[idx_exon_skip[k]]
 
@@ -175,7 +175,7 @@ def collect_events(CFG):
                 ### detect alternative intron_ends from splicegraph
                 if do_alt_3prime or do_alt_5prime:
                     if not os.path.exists(fn_out_a5) or not os.path.exists(fn_out_a3):
-                        idx_alt_end_5prime, exon_alt_end_5prime, idx_alt_end_3prime, exon_alt_end_3prime = detect_altprime(genes, sp.where([x.is_alt for x in genes])[0])
+                        idx_alt_end_5prime, exon_alt_end_5prime, idx_alt_end_3prime, exon_alt_end_3prime = detect_events(genes, 'alt_prime', sp.where([x.is_alt for x in genes])[0], CFG)
                         ### handle 5 prime events
                         for k in range(len(idx_alt_end_5prime)):
                             gene = genes[idx_alt_end_5prime[k]]
@@ -280,7 +280,7 @@ def collect_events(CFG):
                 ### detect multiple_exon_skips from splicegraph
                 if do_mult_exon_skip:
                     if not os.path.exists(fn_out_mes):
-                        idx_mult_exon_skip, exon_mult_exon_skip, id_mult_exon_skip = detect_multipleskips(genes, sp.where([x.is_alt for x in genes])[0])
+                        idx_mult_exon_skip, exon_mult_exon_skip, id_mult_exon_skip = detect_events(genes, 'mult_exon_skip', sp.where([x.is_alt for x in genes])[0], CFG)
                         if len(id_mult_exon_skip) > 0:
                             for k in id_mult_exon_skip:
                                 #k_ = sp.where(sp.array(id_mult_exon_skip) == k)[0]
@@ -316,7 +316,7 @@ def collect_events(CFG):
                 ### detect mutually exclusive exons from splicegraph
                 if do_mutex_exons:
                     if not os.path.exists(fn_out_mex):
-                        idx_mutex_exons, exon_mutex_exons = detect_xorexons(genes, sp.where([x.is_alt for x in genes])[0])
+                        idx_mutex_exons, exon_mutex_exons = detect_events(genes, 'mutex_exons', sp.where([x.is_alt for x in genes])[0], CFG)
                         if len(idx_mutex_exons) > 0:
                             for k in range(len(exon_mutex_exons)):
                                 gene = genes[idx_mutex_exons[k]]
