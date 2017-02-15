ac8477be1e53cb12b60f67adbac41af1afc4fa07
Andre Kahles <andre.kahles@ratschlab.org>
2017-02-15 14:13:53 +0100

    Optimized drawing of splicing graph
    * removed unnecessary redundancy
    * use patches for drawing

--- python/modules/viz/graph.py
+++ python/modules/viz/graph.py
@@ -1,6 +1,9 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib.lines as mlines
+from matplotlib.collections import PatchCollection
 import cPickle
 import sys
 import scipy as sp
@@ -18,10 +21,10 @@ def plot_graph(vertices, edges, ax, xlim=None, highlight=None, highlight_color='
     ax.yaxis.grid(False)
 
     ### nodes
-    nodes = []
+    patchlist = []
     exon_num = sp.zeros((stop - start,))
     exon_loc = sp.zeros((1, stop - start))
-    exon_level = sp.zeros((vertices.shape[1], vertices.shape[1]))
+    exon_level = sp.zeros((vertices.shape[1], )) #vertices.shape[1]))
     for i in range(vertices.shape[1]):
         cur_vertex = vertices[:, i] - start
         exon_num[cur_vertex[0]:cur_vertex[1]] += 1
@@ -39,35 +42,40 @@ def plot_graph(vertices, edges, ax, xlim=None, highlight=None, highlight_color='
        
         exon_level[i] = level
         
-        nodes.append(matplotlib.patches.Rectangle([cur_vertex[0] + start, 20 + (level * 20)], cur_vertex[1] - cur_vertex[0], 10, facecolor=node_color, edgecolor='none', alpha=0.7))
-
+        patchlist.append(mpatches.Rectangle([cur_vertex[0] + start, 20 + (level * 20)], cur_vertex[1] - cur_vertex[0], 10, facecolor=node_color, edgecolor='none', alpha=0.7))
 
     ### edges
+    linelist = []
     intron_loc = sp.zeros((1, stop - start))
     if edges.shape[0] > 1:
-        for i in range(vertices.shape[1]):
-            for j in range(i + 1, vertices.shape[1]):
-                if edges[i ,j] > 0:
-                    if vertices[0,i] < vertices[0,j]:
-                        istart = vertices[1, i]
-                        istop = vertices[0, j]
-                        level1 = exon_level[i]
-                        level2 = exon_level[j]
-                    else:
-                        istart = vertices[1, j]
-                        istop = vertices[0, i]
-                        level1 = exon_level[j]
-                        level2 = exon_level[i]
-              
-                    cur_intron = [istart - start, istop - start]
-                    intron_loc[cur_intron[0]:cur_intron[1]] += 1
-                    leveli = [(istart + istop) * 0.5, (level1 + level2) * 0.5]
-                    ax.plot([istart, leveli[0]], [25 + (level1 * 20), 32 + (leveli[1] * 20)], '-', color=edge_color, linewidth=0.5)
-                    ax.plot([leveli[0], istop], [32 + (leveli[1] * 20), 25 + (level2 * 20)], '-', color=edge_color, linewidth=0.5)
+        ii, jj = sp.where(sp.triu(edges) > 0)
+        for i, j in zip(ii, jj):
+        #for i in range(vertices.shape[1]):
+            #for j in range(i + 1, vertices.shape[1]):
+                #if edges[i ,j] > 0:
+            if vertices[0,i] < vertices[0,j]:
+                istart = vertices[1, i]
+                istop = vertices[0, j]
+                level1 = exon_level[i]
+                level2 = exon_level[j]
+            else:
+                istart = vertices[1, j]
+                istop = vertices[0, i]
+                level1 = exon_level[j]
+                level2 = exon_level[i]
+      
+            cur_intron = [istart - start, istop - start]
+            intron_loc[cur_intron[0]:cur_intron[1]] += 1
+            leveli = [(istart + istop) * 0.5, (level1 + level2) * 0.5]
+            #ax.plot([istart, leveli[0], istop], [25 + (level1 * 20), 32 + (leveli[1] * 20), 25 + (level2 * 20)], '-', color=edge_color, linewidth=0.5)
+            linelist.append(mlines.Line2D([istart, leveli[0], istop], [25 + (level1 * 20), 32 + (leveli[1] * 20), 25 + (level2 * 20)], color=edge_color, linewidth=0.5))
+            #ax.plot([leveli[0], istop], [32 + (leveli[1] * 20), 25 + (level2 * 20)], '-', color=edge_color, linewidth=0.5)
 
-    ### draw nodes 
-    for node in nodes:
+    ### draw patches 
+    for node in patchlist:
         ax.add_patch(node)
+    for line in linelist:
+        ax.add_line(line)
 
     ### axes 
     if xlim is not None:
