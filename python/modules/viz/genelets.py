ec98e15cea38a6fb75bc20351112738c563d250e
akahles <andre.kahles@inf.ethz.ch>
2018-03-20 10:29:08 +0100

    allow for padding in genelet plots

--- python/modules/viz/genelets.py
+++ python/modules/viz/genelets.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 import scipy as sp
 
-def single(exons, ax=None, x_range=None, count=1, color='blue', grid=False, label=None):
+def single(exons, ax=None, x_range=None, count=1, color='blue', grid=False, label=None, padding=None):
 
     if ax is None:
         fig = plt.figure(figsize = (10, 4))
@@ -26,6 +26,9 @@ def single(exons, ax=None, x_range=None, count=1, color='blue', grid=False, labe
         else:
             min_ex = min(min_ex, exon[0])
             max_ex = max(max_ex, exon[1] + 1)
+        if not padding is None:
+            min_ex = max(min_ex - padding, 0)
+            max_ex += padding
         rect = matplotlib.patches.Rectangle((exon[0], -20 - ((count - 1) * 25)), exon[1] - exon[0] + 1, 10, facecolor=color, edgecolor='none', alpha=0.7)
         ax.add_patch(rect)
         if e_idx > 0:
@@ -44,14 +47,14 @@ def single(exons, ax=None, x_range=None, count=1, color='blue', grid=False, labe
 
     ax.get_yaxis().set_visible(False)
 
-def multiple(exon_set, ax=None, x_range=None, color='blue', labels=None, grid=None):
+def multiple(exon_set, ax=None, x_range=None, color='blue', labels=None, grid=None, padding=None):
 
     if labels is not None:
         assert len(labels) == len(exon_set), 'Number of labels and given genelet entities must be identical!\nlen(labels) = %i != len(entities) = %i' % (len(labels), len(exon_set))
 
     for e_idx, exons in enumerate(exon_set):
         if labels is not None:
-            single(exons, ax=ax, x_range=x_range, color=color, count=e_idx + 1, grid=grid, label=labels[e_idx])
+            single(exons, ax=ax, x_range=x_range, color=color, count=e_idx + 1, grid=grid, label=labels[e_idx], padding=padding)
         else:
-            single(exons, ax=ax, x_range=x_range, color=color, count=e_idx + 1, grid=grid)
+            single(exons, ax=ax, x_range=x_range, color=color, count=e_idx + 1, grid=grid, padding=padding)
 
