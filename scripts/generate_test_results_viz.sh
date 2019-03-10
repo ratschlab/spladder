c9ffc54e1b79c4d94d6b0283fb5502d8396c6306
akahles <andre.kahles@inf.ethz.ch>
2019-03-10 12:48:39 +0100

    updated test cases

--- scripts/generate_test_results_viz.sh
+++ scripts/generate_test_results_viz.sh
@@ -13,6 +13,6 @@ for testcase in $testcases
 do
     outdir=tests/testcase_${testname}/results_merged_${testcase}
     mkdir -p $outdir
-    python -m spladder.spladder viz -o ${outdir} -b ${datadir}/align/${testcase}_1.bam,${datadir}/align/${testcase}_2.bam,${datadir}/align/${testcase}_3.bam:${datadir}/align/${testcase}_4.bam,${datadir}/align/${testcase}_5.bam -L group1,group2 -f png
+    python -m spladder.spladder viz -o ${outdir} -b ${datadir}/align/${testcase}_1.bam,${datadir}/align/${testcase}_2.bam,${datadir}/align/${testcase}_3.bam:${datadir}/align/${testcase}_4.bam,${datadir}/align/${testcase}_5.bam -L group1,group2 -f ps
 done
 
