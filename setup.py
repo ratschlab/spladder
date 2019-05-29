e232978131eb04ffebe7433cfbb229020860d442
akahles <andre.kahles@inf.ethz.ch>
2019-05-29 10:24:21 +0200

    changed release content type for README

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
     install_requires=requirements,
     license="BSD license",
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='spladder',
     name='spladder',
