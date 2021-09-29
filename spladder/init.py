58f1cf744c11325b4b63759467f00b55c2ab2c04
akahles <andre.kahles@inf.ethz.ch>
2021-09-29 14:02:56 +0200

    slightly improved readability

--- spladder/init.py
+++ spladder/init.py
@@ -90,6 +90,9 @@ def init_genes_gtf(options):
             continue
         sl = line.strip().split('\t')
         
+        trans_id = tags['transcript_id']
+        gene_id = tags['gene_id']
+
         ### get start and end
         try:
             start = int(sl[3]) - 1
@@ -103,34 +106,36 @@ def init_genes_gtf(options):
         ### get tags
         tags = get_tags_gtf(sl[8])
 
-        ### add exons
-        if sl[2] in ['exon', 'Exon']:
-            trans_id = tags['transcript_id']
-            gene_id = tags['gene_id']
-            try:
-                t_idx = genes[gene_id].transcripts.index(trans_id)
-            except ValueError:
-                t_idx = len(genes[gene_id].transcripts)
-                genes[gene_id].transcripts.append(trans_id)
-            except KeyError:
-                if 'gene_type' in tags:
-                    gene_type = tags['gene_type']
-                elif 'gene_biotype' in tags:
-                    gene_type = tags['gene_biotype']
-                else:
-                    gene_type = None
+        ### only use exon lines
+        if sl[2].lower() != 'exon':
+            continue
 
-                warn_infer_count += 1
-                if warn_infer_count < 5:
-                    print('WARNING: %s does not have gene level information for transcript %s - information has been inferred from tags'  % (options.annotation, trans_id), file=sys.stderr)
-                elif warn_infer_count == 5:
-                    print('WARNING: too many warnings for inferred tags', file=sys.stderr)
-                    
-                genes[gene_id] = Gene(name=gene_id, start=start, stop=stop, chr=sl[0], strand=sl[6], source=sl[1], gene_type=gene_type)
-                t_idx = len(genes[gene_id].transcripts)
-                genes[gene_id].transcripts.append(trans_id)
-                inferred_genes = True
-            genes[gene_id].add_exon(np.array([int(sl[3]) - 1, int(sl[4])], dtype='int'), idx=t_idx)
+        ### infer transcript ID
+        try:
+            t_idx = genes[gene_id].transcripts.index(trans_id)
+        except ValueError:
+            t_idx = len(genes[gene_id].transcripts)
+            genes[gene_id].transcripts.append(trans_id)
+        except KeyError:
+            if 'gene_type' in tags:
+                gene_type = tags['gene_type']
+            elif 'gene_biotype' in tags:
+                gene_type = tags['gene_biotype']
+            else:
+                gene_type = None
+
+            warn_infer_count += 1
+            if warn_infer_count < 5:
+                print('WARNING: %s does not have gene level information for transcript %s - information has been inferred from tags'  % (options.annotation, trans_id), file=sys.stderr)
+            elif warn_infer_count == 5:
+                print('WARNING: too many warnings for inferred tags', file=sys.stderr)
+                
+            genes[gene_id] = Gene(name=gene_id, start=start, stop=stop, chr=sl[0], strand=sl[6], source=sl[1], gene_type=gene_type)
+            t_idx = len(genes[gene_id].transcripts)
+            genes[gene_id].transcripts.append(trans_id)
+            inferred_genes = True
+
+        genes[gene_id].add_exon(np.array([start, stop], dtype='int'), idx=t_idx)
 
     ### post-process in case we have inferred genes
     if warn_infer_count >= 5:
