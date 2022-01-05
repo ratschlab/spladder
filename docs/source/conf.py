3e4b2dcb792ceb5ca6d421618d3ee3f601bdb943
akahles <andre.kahles@inf.ethz.ch>
2022-01-05 13:56:18 +0100

    bump version in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2019, Andre Kahles'
 author = u'Andre Kahles'
 
 # The short X.Y version
-version = u'2.4'
+version = u'2.5'
 # The full version, including alpha/beta/rc tags
-release = u'2.4.2'
+release = u'2.5.0'
 
 
 # -- General configuration ---------------------------------------------------
