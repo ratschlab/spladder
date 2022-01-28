356bb8fd1902fa60c224a077845c4f7a912b2917
Andre Kahles <andre.kahles@inf.ethz.ch>
2022-01-28 11:55:17 +0100

    version bump

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ratschlab/spladder',
-    version='3.0.1',
+    version='3.0.2',
     zip_safe=False,
 )
